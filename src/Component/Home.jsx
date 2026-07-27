4b909b85400087167669514adf69d1cfb3903eb1
Salfartech <Salfartechmedia@gmail.com>
2026-07-28 00:31:10 +0530

    ff

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -25,13 +25,12 @@ const Home = () => {
       <AboutSection />
       <GalleryHome />
 
-      {/* <SpecialtiesSection /> */}
-      {/* <Consultant /> */}
+      <SpecialtiesSection />
       <AppointmentCTA />
 
       {/* <!-- Elfsight Google Reviews | Untitled Google Reviews --> */}
-      <script src="https://elfsightcdn.com/platform.js" async></script>
-      <div class="elfsight-app-ddb813d5-5c55-4316-ab25-635783e96df6" data-elfsight-app-lazy></div>
+      {/* <script src="https://elfsightcdn.com/platform.js" async></script>
+      <div class="elfsight-app-ddb813d5-5c55-4316-ab25-635783e96df6" data-elfsight-app-lazy></div> */}
 
       {/* <PreFooterCTA /> */}
       <Contact />
